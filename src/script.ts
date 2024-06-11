fa2995a2fda9acc42afe03b73992875e352e245c
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-06-11 12:59:08 +0200

    First version with timeout

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,17 @@
-function throttle() {
-    console.log('function called');
+function throttle(func: (...args: any[]) => void, delay: number) {
+    let timeout: NodeJS.Timeout | null = null
+    return (...args: any[]) => {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+        timeout = setTimeout(() => {
+            func(...args);
+        }, delay);
+    };
 }
 
-export { throttle }
+function addNewItem(array: string[], item: string) {
+    array.push(item);
+}
+
+export { addNewItem, throttle }
--- tests/script.test.ts
+++ tests/script.test.ts
@@ -1,8 +1,9 @@
-import { throttle } from '../src/script';
+import { addNewItem, throttle } from '../src/script';
 
-describe('Sample Test', () => {
-    throttle();
-    it('should return true', () => {
-        expect(true).toBe(true);
-    });
+describe('It should execute only once, 1s after the last call', () => {
+    let array: string[] = [];
+    const throttledAddNewItem = throttle(() => addNewItem(array, 'done'), 1000);
+    throttledAddNewItem();
+    jest.advanceTimersByTime(1000);
+    expect(array).toEqual(['done']);
 });
