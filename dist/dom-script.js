04b9dd6149a8c28d1db5cb3007bd2f9e4dacc6a8
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-06-12 12:56:43 +0200

    Copy throttle function instead of importing it

--- dist/dom-script.js
+++ dist/dom-script.js
@@ -1,5 +1,3 @@
-import {throttle} from './script.js';
-
 // DOM elements
 defaultCounter = document.getElementById('default-counter');
 throttledCounter = document.getElementById('throttled-counter');
@@ -7,7 +5,7 @@ throttledCounter = document.getElementById('throttled-counter');
 // Listeners
 document.addEventListener('mousemove', e => {
     incrementCount(defaultCounter);
-    throttledCounter();
+    throttlingCounter();
 })
 
 // Functions
@@ -15,4 +13,34 @@ function incrementCount(element) {
     element.textContent = (parseInt(element.innerText) || 0) + 1;
 }
 
-const throttledCounter = throttle(() => incrementCount(throttledCounter), 500);
+function throttle(func, delay) {
+    var isWaiting = false;
+    var waitingInput;
+    var timeout = function () {
+        if (waitingInput == null) {
+            isWaiting = false;
+        }
+        else {
+            func(waitingInput);
+            waitingInput = null;
+            setTimeout(timeout, delay);
+        }
+    };
+    return function () {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i] = arguments[_i];
+        }
+        if (isWaiting) {
+            waitingInput = args;
+            return;
+        }
+        else {
+            func.apply(void 0, args);
+            isWaiting = true;
+            setTimeout(timeout, delay);
+        }
+    };
+}
+
+const throttlingCounter = throttle(() => incrementCount(throttledCounter), 500);
